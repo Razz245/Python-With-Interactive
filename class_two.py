9d4b0dd337de035137da4bc76349cc754137ac11
Razz245 <infovknews24@gmail.com>
2025-10-26 03:44:49 +0600

    Initial commit: class one and class two interactive files

---
+++ class_two.py
@@ -0,0 +1,99 @@
+#Topic Five: Data Types and Variables
+
+"""Data Types বলে দেয় একটি variable কী ধরনের মান (value) ধারণ করছে।
+Python-এ সবকিছুই object, আর প্রতিটি object-এর একটি নির্দিষ্ট data type থাকে।"""
+
+
+#Common Built-in Data Types in Python
+# 1. Numeric Types: int, float, complex
+# 2. Sequence Types: list, tuple, range
+# 3. Text Type: str
+# 4. Set Types: set, frozenset
+"""
+Data Type	Example	Description
+int	x = 10	Whole number (integer)
+float	y = 10.5	Decimal number
+str	name = "Rajib"	Text or string
+bool	is_hacker = True	Boolean (True/False)
+list	fruits = ["apple", "mango", "banana"]	Ordered, changeable, allows duplicates
+tuple	colors = ("red", "green", "blue")	Ordered, unchangeable
+set	nums = {1, 2, 3}	Unordered, no duplicates
+dict	person = {"name": "Rajib", "age": 25}	Key-value pairs
+"""
+
+#Variable Naming Conventions and Best Practices
+
+#Checking Data Type
+x = 10
+y = "Hello"
+z = [1, 2, 3]
+print(type(x))   # <class 'int'>
+print(type(y))   # <class 'str'>
+print(type(z))   # <class 'list'>
+
+#Type Casting (Convert one type to another)
+x = int(5.9)         # float → int
+y = float(10)        # int → float
+z = str(100)         # int → string
+a = list((1, 2, 3))  # tuple → list
+
+#Type Casting (Convert one type to another)
+x = int(5.9)         # float → int
+y = float(10)        # int → float
+z = str(100)         # int → string
+a = list((1, 2, 3))  # tuple → list
+
+#Examples of Each Type
+
+#string
+name = "Python"
+print(name.upper())  # PYTHON
+
+#Integer & Float
+a = 10
+b = 3.5
+print(a + b)  # 13.5
+
+#Boolean
+x = True
+y = False
+print(x and y)  # False
+
+#List
+my_list = [10, 20, 30]
+my_list.append(40)
+print(my_list)  # [10, 20, 30, 40]
+
+#Tuple
+coordinates = (10, 20)
+print(coordinates[0])  # 10
+
+#Set
+unique_numbers = {1, 2, 3, 3, 2}
+print(unique_numbers)  # {1, 2, 3}
+
+#Dictionary
+student = {"name": "Rajib", "age": 25, "subject": "Python"}
+print(student["name"])  # Rajib
+
+#Example:
+# Mutable
+numbers = [1, 2, 3]
+numbers.append(4)
+print(numbers)  # [1, 2, 3, 4]
+
+# Immutable
+name = "Rajib"
+name = name + " Sharma"
+print(name)  # Rajib Sharma
+
+#Mixed Example
+person = {
+    "name": "Rajib",
+    "age": 25,
+    "skills": ["Python", "Linux", "Cybersecurity"],
+    "active": True
+}
+
+print(person["skills"][1])  # Linux
+print(type(person))         # <class 'dict'>
---
+++ classone_run_first.py
@@ -0,0 +1,93 @@
+#First Class first code
+print("This is the first class code running.")
+
+#vlass two code
+#python arithmetic operations
+#python debugging example
+X = 10
+Y = 20
+sum = X + Y
+sub=Y - X
+mul=X * Y
+div=Y / X
+mod=Y % X
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print("End of first class code.")
+
+#python comments example
+# - This is a single line comment
+"""This is a multi-line comment"""
+
+#python variable example
+total_price = 100
+print(total_price) #printing variable value
+#Use Descriptive variable names
+#Lowercase with underscores for variable names
+#avoid using reserved keywords as variable names
+#start variable names with letters or underscores, not numbers
+#case-sensitive variable names
+#short and meaningful variable names
+#avoid special characters in variable names
+#consistent naming conventions
+#initialize variables before use
+#use comments to explain complex variable usage
+#avoid using global variables when possible
+#use constants for values that do not change
+#follow language-specific naming conventions
+#Ude sing camelCase or PascalCase for variable names in certain languages
+#use singular nouns for variable names representing single entities
+#avoif double underscores in variable names unless necessary
+user_name = "Rajib"
+age1 = 25
+country = "Bangladesh"
+
+"""
+1age = 25       # starts with a number ❌
+user name = "Rajib"  # space ❌
+for = 10        # keyword ❌
+""" 
+
+"""Variable Types (by value)"""
+
+x = 10          # int
+y = 10.5        # float
+name = "Rajib"  # string
+is_hacker = True # boolean
+fruits = ["apple", "mango"]  # list
+
+"""Changing Variable Value:"""
+x = 5
+x = 10   # value changed
+print(x) # output → 10
+
+"""Assigning Multiple Variables:"""
+a, b, c = 10, 20, 30
+print(a, b, c)   # 10 20 30
+
+""""Type Casting (Converting Variable Type):"""
+x = int(5.5)       # float → int
+y = str(100)       # int → string
+z = float("5.5")   # string → float
+
+# Checking Variable Type:
+a = 10
+b = "Hello"
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'str'>
+
+
+#Exzmplr Variables in Python Practice
+# Variable Demo
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = "Dhaka"
+
+print(f"Hello {name}, you are {age} years old and live in {city}.")
+
+
+
